1357587a85d4963420f695e0cbb4ceb25a44b0fc
Cipto Hadi <ciptohadi79@gmail.com>
2020-10-10 12:00:31 +0700

    initiliaze the firebase config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import firebase from './firebase';
+console.log(firebase);
 
 function App() {
-  return <div></div>;
+  return <div>Awesome!</div>;
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDckJBJ-ivjGPXp3a1QfFs5fbygcTE99xk",
+  authDomain: "nwitter-c0014.firebaseapp.com",
+  databaseURL: "https://nwitter-c0014.firebaseio.com",
+  projectId: "nwitter-c0014",
+  storageBucket: "nwitter-c0014.appspot.com",
+  messagingSenderId: "1018637882355",
+  appId: "1:1018637882355:web:6589d0a29933668dce7886",
+  measurementId: "G-VZZY3WZS96",
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
